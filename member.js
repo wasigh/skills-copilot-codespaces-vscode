9151587ab47c9fb0df54aca85100712d652a5eb8
Hans van Leuken <hansvanleuken@gmail.com>
2024-12-27 14:57:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    const skills = [
+        'JavaScript',
+        'React',
+        'Node.js',
+        'MongoDB',
+        'Express',
+        'HTML',
+        'CSS',
+        'Python',
+        'C++',
+        'C#',
+        'Java',
+        'SQL',
+        'Git',
+        'Bash',
+        'Linux',
+        'Windows'
+    ];
+
+    return skills;
+}
